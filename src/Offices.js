3cd760cd4eb7d207bd236df48254ea6cf2168dfa
Mark Allen <markallen.webdeveloper@gmail.com>
2019-11-25 14:59:19 +0000

    Sorted gutter issue. Added data to cards. Added buttons to cards. Adding loading spinner to data pages

--- src/Offices.js
+++ src/Offices.js
@@ -3,6 +3,7 @@
 
 import React, { Component } from "react";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 
 class Offices extends Component {
   constructor() {
@@ -25,38 +26,70 @@ class Offices extends Component {
   }
 
   render() {
-    let officeCards = "";
+    let officeCards = (
+      // Default data loading spinner
+      <div className="col-sm-12 text-center">
+        <Loader
+          type="Circles"
+          color="#FFFFFF"
+          height={150}
+          width={150}
+          timeout={0} // Show until data loads
+        />
+      </div>
+    );
+
     // Handle rendering before DB response received
     if (Object.keys(this.state.data).length > 0) {
-      console.log(`B: ${this.state.data}`);
-      officeCards = this.state.data.map((data) => (
-        <div className="col-sm-4">
-          <div className="card h-100 card-spacing">
+      officeCards = this.state.data.map(data => (
+        <div className="col-sm-4" key={data.office}>
+          <div className="card card-spacing">
             <div className="card-body">
               <h5 className="card-title">{data.office}</h5>
-              <p className="card-text">
-                Bespoke responsive websites and web applications written to your
-                specifications. Hand coded using HTML5, CSS3, Javascript, React
-                and Bootstrap.
-              </p>
+              <div className="card-text">
+                {"Building: " +
+                  (data.building.length > 0 ? data.building : "---")}
+              </div>
+              <div className="card-text">
+                {"Number: " + (data.number.length > 0 ? data.number : "---")}
+              </div>
+              <div className="card-text">
+                {"Street: " + (data.street.length > 0 ? data.street : "---")}
+              </div>
+              <div className="card-text">{"City: " + data.town}</div>
+              <div className="card-text">{"Postcode: " + data.postcode}</div>
+            </div>
+            <div className="row justify-content-center">
+              <div className="">
+                <button type="button" className="btn btn-update btn-circle">
+                  <i className="fas fa-pencil-alt"></i>
+                </button>
+              </div>
+              <div className="text-center">
+                <button type="button" className="btn btn-delete btn-circle">
+                  <i className="fas fa-trash"></i>
+                </button>
+              </div>
             </div>
           </div>
         </div>
       ));
-    } else {
-      console.log(`B: No data`);
     }
     return (
       <div className="App-body App-offices pb-5">
         <div className="container App-content">
-          <div className="row justify-content-center padding-bottom-5vh">
-            <div className="col-xs-12">
+          <div className="row">
+            <div className="col-sm-2"></div>
+            <div className="col-sm-8 text-center">
               <h2>Office records</h2>
             </div>
+            <div className="col-sm-2 text-right">
+              <button type="button" className="btn btn-create btn-circle">
+                <i className="fas fa-plus"></i>
+              </button>
+            </div>
           </div>
-          <div className="row padding-bottom-5vh">
-            {officeCards}
-          </div>
+          <div className="row padding-bottom-5vh">{officeCards}</div>
           <div className="row pb-1"></div>
         </div>
       </div>
--- src/Staff.js
+++ src/Staff.js
@@ -2,167 +2,90 @@
 // M. Allen - 2019
 
 import React, { Component } from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
 
 class Services extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.getEmployeeData();
+  }
+
+  getEmployeeData() {
+    axios.get(this.props.employeeDataURL).then(res => {
+      this.setState({
+        data: res.data
+      });
+    });
+  }
+
   render() {
+    let employeeCards = (
+      // Default data loading spinner
+      <div className="col-sm-12 text-center">
+        <Loader
+          type="Circles"
+          color="#FFFFFF"
+          height={150}
+          width={150}
+          timeout={0} // Show until data loads
+        />
+      </div>
+    );
+
+    // Handle rendering before DB response received
+    if (Object.keys(this.state.data).length > 0) {
+      employeeCards = this.state.data.map(data => (
+        <div className="col-sm-4" key={data.office}>
+          <div className="card card-spacing">
+            <div className="card-body">
+              <h5 className="card-title">
+                {data.firstName + " " + data.lastName}
+              </h5>
+              <div className="card-text">{"Position: " + data.position}</div>
+              <div className="card-text">{"Office: " + data.office}</div>
+              <div className="card-text">{"ID: " + data.staffId}</div>
+              <div className="card-text">{"Telephone: " + data.telephone}</div>
+              <div className="card-text">{"Email: " + data.email}</div>
+            </div>
+            <div className="row justify-content-center">
+              <div className="">
+                <button type="button" className="btn btn-update btn-circle">
+                  <i className="fas fa-pencil-alt"></i>
+                </button>
+              </div>
+              <div className="text-center">
+                <button type="button" className="btn btn-delete btn-circle">
+                  <i className="fas fa-trash"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ));
+    }
     return (
       <div className="App-body App-staff pb-5">
         <div className="container App-content">
-        <div className="row justify-content-center padding-bottom-5vh">
-            <div className="col-xs-12">
-              <h2>Staff records</h2>
+          <div className="row">
+          <div className="col-sm-2"></div>
+            <div className="col-sm-8 text-center">
+              <h2>Employee records</h2>
+            </div>
+            <div className="col-sm-2 text-right">
+              <button type="button" className="btn btn-create btn-circle">
+                <i className="fas fa-plus"></i>
+              </button>
             </div>
           </div>
-          <div className="row pb-1">
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fas fa-code card-icon-1"></i> Hand-crafted web apps</h5>
-                  <p className="card-text">
-                    Bespoke responsive websites and web applications written to
-                    your specifications. Hand coded using HTML5, CSS3,
-                    Javascript, React and Bootstrap.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fas fa-file-alt card-icon-2"></i> Template based websites</h5>
-                  <p className="card-text">
-                    Professional modern websites developed using Bootstrap
-                    templates, modified and enhanced to meet your needs. Choose
-                    from hundreds of themes.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fab fa-wordpress card-icon-3"></i> Wordpress websites</h5>
-                  <p className="card-text">
-                    Professional websites created using Wordpress.org templates.
-                    Choose from hundreds of free and chargeable commercial
-                    themes.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row pb-1">
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fas fa-users card-icon-4"></i> Remote team member</h5>
-                  <p className="card-text">
-                    Hire me to be a remote working member of your development
-                    team. Short or long-term, part-time or full-time. Get in
-                    touch to discuss your requirements.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fas fa-headset card-icon-1"></i> Technical support</h5>
-                  <p className="card-text">
-                    With many years experience of installing and supporting
-                    software, I can help you solve technical problems and
-                    provide ongoing support.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fas fa-pen card-icon-2"></i> Documentation</h5>
-                  <p className="card-text">
-                    Clear documentation cuts support costs, improves the user
-                    experience, and increases customer satisfaction.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row pb-1">
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fas fa-feather-alt card-icon-3"></i> Bespoke web content</h5>
-                  <p className="card-text">
-                    Professional bespoke web content written to enhance your web
-                    presence and communicate your key messages to your
-                    customers.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fas fa-pound-sign card-icon-4"></i> Low-cost websites</h5>
-                  <p className="card-text">
-                    Budget websites for micro-businesses, charities and
-                    community groups, created and hosted using the Weebly or Wix
-                    website builders.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fas fa-wrench card-icon-1"></i> Changes, updates &amp; fixes</h5>
-                  <p className="card-text">
-                    Changes to existing web content, updates to site layout,
-                    changes to functionality, adding new features, and fixing
-                    problems.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row pb-1">
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fas fa-poll-h card-icon-2"></i> Questback / Unipark EFS</h5>
-                  <p className="card-text">
-                    Custom HTML, CSS, and Javascript for Questback / Unipark EFS
-                    questions including type 911 user-defined questions.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fab fa-linux card-icon-3"></i> Linux administration</h5>
-                  <p className="card-text">
-                    Linux systems administration, installation, configuration,
-                    performance monitoring, capacity planning, and problem
-                    solving.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className="card h-100">
-                <div className="card-body">
-                  <h5 className="card-title"><i className="fas fa-balance-scale-right card-icon-4"></i> Software evaluation</h5>
-                  <p className="card-text">
-                    Researching and reviewing available software packages and
-                    technologies to identify the best solution to meet your
-                    business needs.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <div className="row padding-bottom-5vh">{employeeCards}</div>
+          <div className="row pb-1"></div>
         </div>
       </div>
     );
